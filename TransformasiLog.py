12f3e372c74871c764697cd4be2b2ceef7b143be
priscillagabriel <86583697+priscillagabriel@users.noreply.github.com>
2021-06-28 12:04:30 +0800

    Add files via upload

---
+++ TransformasiLog.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread('Danau.jpg')
+
+c = 255 / np.log(1 + np.max(image))
+log_image = c * (np.log(image + 1))
+
+log_image = np.array(log_image, dtype=np.uint8)
+
+plt.imshow(image)
+plt.show()
+plt.imshow(log_image)
+plt.show()
